48431ebe5d6ce2cc86d818b5519d49eb293c5d20
Flashvita <vitalijkulesov975@gamil.com>
2022-04-29 08:24:13 +0300

    Control

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=30, verbose_name='Категория')
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
